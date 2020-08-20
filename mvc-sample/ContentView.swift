731895710dbdce6138a4aadf8b80fc8b945b3dbb
spolyak <stevepolyak@mac.com>
2020-08-20 12:57:08 -0500

    Initial Commit

---
+++ mvc-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mvc-sample
+//
+//  Created by Steve Polyak on 8/20/20.
+//  Copyright © 2020 Steve Polyak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
